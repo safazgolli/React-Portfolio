037aa3cc83afd8b04ea9a123cdd7ef35438eb063
Safa Zgolli <fifi.zgolli89@gmail.com>
2022-11-22 12:57:44 -0500

    Creating a PR for testing

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import PortfolioContainer from "./components/portfolioContainer";
 import Footer from "./components/Footer";
-
+const App = 12
 function App() {
   return (
  
