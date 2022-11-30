aed2798ed67545c02929f3fd389e28d98a003c65
Safa Zgolli <fifi.zgolli89@gmail.com>
2022-11-29 23:28:57 -0500

    Updates

---
+++ precache-manifest.d5df7adeb3b979d22d7e16e846f520e8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3c670c7b13166410d3b",
+    "url": "/react-portfolio/static/css/main.60743733.chunk.css"
+  },
+  {
+    "revision": "c3c670c7b13166410d3b",
+    "url": "/react-portfolio/static/js/main.c3c670c7.chunk.js"
+  },
+  {
+    "revision": "7367ef01f200e5a9b2f3",
+    "url": "/react-portfolio/static/css/1.a7faa837.chunk.css"
+  },
+  {
+    "revision": "7367ef01f200e5a9b2f3",
+    "url": "/react-portfolio/static/js/1.7367ef01.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "9b55c5f13a39a1dd392bd4a9d75bca0a",
+    "url": "/react-portfolio/static/media/resumecv.9b55c5f1.pdf"
+  },
+  {
+    "revision": "c00172ed01b128d95f2459b2d80a1f8a",
+    "url": "/react-portfolio/static/media/me.c00172ed.jpeg"
+  },
+  {
+    "revision": "e3eca0c6d0c19d0631b702789c3436a8",
+    "url": "/react-portfolio/static/media/jsQuiz.e3eca0c6.png"
+  },
+  {
+    "revision": "a978cd7770c61b52e23521b57e907147",
+    "url": "/react-portfolio/static/media/weatherDashboard.a978cd77.png"
+  },
+  {
+    "revision": "121914eb8e1a4eb0c1d486865cd667aa",
+    "url": "/react-portfolio/static/media/tech-blog.121914eb.png"
+  },
+  {
+    "revision": "9b4fca9daaddc84d124031931482df52",
+    "url": "/react-portfolio/static/media/eventplaner.9b4fca9d.png"
+  },
+  {
+    "revision": "4c90253290f3a15e9e47a802a8d4d96b",
+    "url": "/react-portfolio/static/media/nas.4c902532.png"
+  },
+  {
+    "revision": "63c625b726a4ff0654d93450747aa8fc",
+    "url": "/react-portfolio/index.html"
+  }
+];
